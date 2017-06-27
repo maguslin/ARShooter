4daf7971b8d1da69f9adfc14c85a5083af3312b7
maguslin <lchen36@gmail.com>
2017-06-28 00:26:55 +0800

    增加基本物件类

---
+++ Assets/Scripts/Core/SceneObject/SceneUnit.cs
@@ -0,0 +1,230 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneUnit : MonoBehaviour {
+	/// <summary>
+	///这些值先写死，以后从表格里读
+	/// </summary>
+	public int id;
+	public float intimacy;//亲密度
+	public float hungry;//饱食度
+	public float m_timestamp;
+	public Transform thisT;
+	public GameObject thisObj;
+	public UnitClassType m_Type;
+	public int baseId;
+	public bool dead = false;
+	/// <summary>
+	/// 这是游戏对象每帧对于设备的transform
+	/// </summary>
+	public Matrix4x4 m_deviceT = new Matrix4x4();
+
+	public virtual bool IsCat() { return false; }
+	public virtual void Init(int baseID)
+	{
+		BaseInit(baseID);
+	}
+
+	public virtual characterBase GetBaseData() { return null; }
+	protected void BaseInit(int baseID)
+	{
+		baseId = baseID;
+		thisT = transform;
+		thisObj = gameObject;
+		LoadAnimationData();
+		LoadSoundData ();
+		dead = false;
+	}
+	protected virtual AIStateManager CreateAIStateManager()
+	{
+		return null;
+	}
+	protected Animation _animation;
+	protected Dictionary<string, animationBase> _AnimationData;
+	protected Dictionary<int, soundBase> _SoundData;
+	public void LoadAnimationData()
+	{
+		_AnimationData = BaseDataManager.Instance.GetAnimationBase(baseId);
+	}
+	public void LoadSoundData()
+	{
+		_SoundData = BaseDataManager.Instance.GetSoundBase (baseId);
+	}
+	public animationBase GetAnimationBaseData(string ani)
+	{
+		if (_AnimationData == null)
+			return null;
+
+		if (!_AnimationData.ContainsKey(ani))
+			return null;
+		return _AnimationData[ani];
+	}
+	struct AniInterval
+	{
+		public string name;
+		public float itv;
+	}
+
+	public virtual void PlayAnimationSeq(params object[] list)//1.name 2.leap
+	{
+		List<AniInterval> anis = new List<AniInterval>();
+		for (int i = 0; i < list.Length; i += 2)
+		{
+			AniInterval ainv = new AniInterval();
+			string aname = (string)list[i];
+			float leapTime = float.Parse(list[i + 1].ToString());
+			ainv.name = aname;
+			ainv.itv = leapTime;
+			anis.Add(ainv);
+		}
+		StartCoroutine(AutoPlayAnimation(anis));
+	}
+	IEnumerator AutoPlayAnimation(List<AniInterval> anis)
+	{
+		for (int i = 0; i < anis.Count; i++)
+		{
+			if (_animation == null)
+			{
+				_animation = GetComponentInChildren<Animation>();
+			}
+			if (_animation.GetClip(anis[i].name) == null) continue;
+			float during = PlayAnimation(anis[i].name);
+			if (_animation[anis[i].name].wrapMode == WrapMode.Loop) break;
+			yield return new WaitForSeconds(during + anis[i].itv);
+		}
+		yield return null;
+	}
+	public string[] GetIdleAnimationList()
+	{
+		return GetBaseData ().idle_list;//idle aniamtions，从表格里读，先写死
+	}
+
+
+	public string GetRandomIdle()
+	{
+		string[] idles = GetIdleAnimationList();
+		if (idles == null)
+		{
+			return "";
+		}
+		return idles[Random.Range(0, idles.Length)];
+	}
+//	public virtual void PlaySound(int soundId)
+//	{
+//		if (!_SoundData.ContainsKey (soundId))
+//			return;
+//		SoundManager.PlaySFX (_SoundData [soundId].file_name,_SoundData [soundId].is_loop);
+//
+//	}
+	public virtual void StopAnimation(string ani)
+	{
+		if (_animation == null)
+		{
+			_animation = GetComponentInChildren<Animation>();
+		}
+		if (_animation == null)
+			return;
+
+		_animation.Stop ();
+	}
+	public virtual float PlayAnimation(string ani)
+	{
+		if (_animation == null)
+		{
+			_animation = GetComponentInChildren<Animation>();
+		}
+		if (_animation == null)
+			return 0.0f;
+		
+		AnimationClip clip = _animation.GetClip(ani);
+		if (clip == null)
+		{
+			//Debug.Log("Can find any clip " + ani);
+
+			return 0;
+		}
+
+		animationBase baseData = GetAnimationBaseData(ani);
+		if (baseData != null)
+		{
+			_animation[ani].speed = baseData.speed_rate;
+		}
+		_animation.Play ();
+		_animation.CrossFade(ani);
+
+		return _animation.clip.length;
+	}
+
+	public virtual float PlayAnimationQueued(string strAnimation)
+	{
+		if (_animation == null)
+		{
+			_animation = GetComponentInChildren<Animation>();
+		}
+		if (_animation == null)
+			return 0.0f;
+
+		AnimationClip clip = _animation.GetClip(strAnimation);
+		if (clip == null)
+		{
+			Debug.Log("Can find any clip " + strAnimation);
+
+			return 0;
+		}
+
+		animationBase baseData = GetAnimationBaseData(strAnimation);
+		if (baseData != null)
+		{
+			_animation[strAnimation].speed = baseData.speed_rate;
+		}
+
+		_animation.CrossFadeQueued(strAnimation);
+
+		return _animation.clip.length;
+	}
+
+	protected GameObject _PresentationObject = null;
+
+	public GameObject GetPresentObject() { return _PresentationObject; }
+	public virtual string GetPrefab() { return GetBaseData().prefab;}//角色的prefab//GetBaseData().prefab; }
+	public void RefreshPresentation()
+	{
+		OnPresentObjectReady(MemoryDataManager.Instance.LoadGameObject(GetPrefab()));
+	}
+	public virtual void OnPresentObjectReady(GameObject obj)
+	{
+		_PresentationObject = obj;
+
+		if (_PresentationObject == null) {
+			//int iiii = 0;
+		}
+		_PresentationObject.transform.parent = thisT;
+
+		_PresentationObject.transform.localPosition = Vector3.zero;
+	}
+	public virtual void OnUnInit(bool immediatly = true)
+	{
+		//RemoveAllAttachEffect();删除特效
+
+		MemoryDataManager.Instance.UnloadGameObject(GetPresentObject());
+
+		if (immediatly)
+			DestroyImmediate(gameObject);
+		else
+			MonoBehaviour.Destroy(gameObject);
+	}
+	public virtual void Update()
+	{
+	}
+	public virtual void FixedUpdate()
+	{
+	}
+	public virtual void Dead()
+	{
+		dead = true;
+
+
+	}
+
+}
---
+++ Assets/Scripts/Core/SceneObject/SceneUnitAI.cs
@@ -0,0 +1,270 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class AIParam
+{
+	
+};
+public enum AIRuningState
+{
+	Running,        //状态正在执行
+	DefaultOver,    //一般Over
+	Over1,          //特殊Over1
+	Over2,         //特殊Over2
+}
+public class AIState
+{
+	protected bool _Executed = false;
+
+	protected AIRuningState _RuningState = AIRuningState.DefaultOver;
+
+	public AIRuningState GetRunningState() { return _RuningState;  }
+
+	public virtual void SetUserData(AIParam data) { }
+	public virtual AIParam GetUserData() { return null; }
+
+	public void Execute()
+	{
+		if (_Executed)
+			return;
+
+		_Executed = true;
+
+		_RuningState = AIRuningState.Running;
+
+		OnExecute( );
+	}
+
+	protected virtual void OnExecute()
+	{
+
+	}
+
+	protected virtual void OnCheck()
+	{
+
+	}
+
+	public void Check()
+	{
+		OnCheck();
+
+		if (GetRunningState() == AIRuningState.DefaultOver)
+		{
+			if (NextState != null)
+			{
+				Manager.SetState(NextState.ToString(), NextState.GetUserData());
+			}
+		}
+
+	}
+	public virtual void Update() { }
+	public virtual void FixedUpdate() { }
+
+	public void Leave()
+	{
+		_Executed = false;
+
+		OnLeave();
+
+		_RuningState = AIRuningState.DefaultOver;
+	}
+
+	protected virtual void OnLeave()
+	{
+
+	}
+
+	public virtual void OnMsg(SceneUnit pSender, MsgType eMsgType, System.Collections.Hashtable arParam)
+	{
+
+	}
+
+	public bool IsKind<T>()
+		where T : AIState
+	{
+		return this.GetType().ToString() == typeof(T).ToString();
+	}
+
+	public AIState NextState
+	{
+		get;
+		set;
+	}
+
+	public AIStateManager Manager
+	{
+		get;
+		set;
+	}
+};
+public class AIStateManager
+{
+	protected Dictionary<string, AIState> _StateList = new Dictionary<string, AIState>();
+
+	protected AIState _CurrentState = null;
+	public void AddState(AIState state)
+	{
+		state.Manager = this;
+		if (_StateList.ContainsKey(state.ToString()))
+		{
+			return;
+		}
+		_StateList.Add(state.ToString(), state);
+	}
+
+	public virtual void LoadAIStates() { }
+
+	public T SetState<T>(AIParam userData = null)
+		where T : AIState
+	{
+		return SetState(typeof(T).ToString(), userData) as T;
+	}
+
+	public T2 SetState<T1,T2>(AIParam userData1 = null,AIParam userData2 = null)
+		where T1:AIState
+		where T2:AIState
+	{
+		AIState destState = GetState<T1>();
+
+		if (destState == null)
+			return null;
+
+		if ( _CurrentState != null )
+			_CurrentState.Leave();
+
+		_CurrentState = destState;
+		_CurrentState.SetUserData(userData1);
+		_CurrentState.Execute();
+
+		T2 nextState = GetState<T2>();
+		_CurrentState.NextState = nextState;
+
+		return nextState;
+
+	}
+
+
+	public bool IsState<T>()
+		where T:AIState
+	{
+		return _CurrentState == GetState<T>();
+	}
+
+	public virtual void Update()
+	{
+		if (_CurrentState != null)
+			_CurrentState.Update();
+	}
+
+	public virtual void FixedUpdate()
+	{
+		if ( _CurrentState != null )
+			_CurrentState.FixedUpdate();
+	}
+
+	//
+	public virtual void Check() 
+	{
+	}
+
+	public T GetState<T>()
+		where T : AIState
+	{
+		string state = typeof(T).ToString();
+		if (!_StateList.ContainsKey(state))
+			return null;
+		return _StateList[state] as T;
+	}
+
+	public AIState SetState(string state, AIParam userData = null)
+	{
+		if (!_StateList.ContainsKey(state))
+			return null;
+
+		if (_CurrentState != null)
+			_CurrentState.Leave();
+
+		_CurrentState = _StateList[state];
+		_CurrentState.SetUserData(userData);
+		_CurrentState.NextState = null;
+		_CurrentState.Execute();
+
+		return _CurrentState;
+	}
+
+	protected float _NextUpdateDelay = 0.5f;
+
+	public void SetNextUpdateDelay(float delay)
+	{
+		_NextUpdateDelay = delay;
+	}
+
+	public virtual float GetNextUpdateDelay()
+	{
+		float delay = _NextUpdateDelay;
+
+		_NextUpdateDelay = 0.2f;
+
+		return delay;
+	}
+
+	public virtual SceneUnit GetBaseParent() { return null; }
+	/*
+     * 说明: 消息传递给状态机处理
+     * 参数:pSender       发送者
+     *          eMsgType    消息ID
+     *          arParam       参数列表
+     */
+	public virtual void OnMsg(SceneUnit pSender, MsgType eMsgType, Hashtable arParam = null)
+	{
+
+	}
+};
+//public class PetAIState : AIState
+//{
+//	public PetAIState()
+//	{
+//
+//	}
+//
+//	public T TParent<T>()
+//		where T :ScenePet
+//	{
+//
+//		ScenePetBaseAIStateManager<T> mgr = Manager as ScenePetBaseAIStateManager<T>;
+//		return mgr.GetParent();
+//	}
+//
+//	public ScenePet Parent
+//	{
+//		get
+//		{
+//			return Manager.GetBaseParent() as ScenePet;
+//		}
+//	}
+//}
+
+//public class ScenePetBaseAIStateManager<T> : AIStateManager
+//	where T : ScenePet
+//{
+//
+//	protected T _Parent;
+//
+//	public ScenePetBaseAIStateManager(T pet)
+//	{
+//		_Parent = pet;
+//
+//		LoadAIStates();
+//	}
+//
+//	public T GetParent() { return _Parent; }
+//
+//
+//	public override SceneUnit GetBaseParent() { return _Parent; }
+//}
+
+
+
